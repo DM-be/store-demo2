6d6742816c9e2e3d8200df40f776290b49640ffa
DM-be <dennis.morent@student.howest.be>
2020-10-13 21:29:52 +0200

    3 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,29 @@ import { FormsModule } from "@angular/forms";
 import { StoreModule } from "@ngrx/store";
 import { AppComponent } from "./app.component";
 
-
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { TodoComponent } from './todo/todo.component';
+import { EffectsModule } from "@ngrx/effects";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { TodoComponent } from "./todo/todo.component";
 import { TodoReducer } from "./store/todo.reducer";
-import { TodoListComponent } from './store/todo-list/todo-list.component';
+import { TodoListComponent } from "./store/todo-list/todo-list.component";
+import { DataService } from "./data.service";
+import { TodoEffects } from "./store/todo.effects";
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    EffectsModule.forRoot([TodoEffects]),
     StoreModule.forRoot(
       {
         todoState: TodoReducer
       },
       {}
     ),
-     StoreDevtoolsModule.instrument(),
+    StoreDevtoolsModule.instrument()
   ],
   declarations: [AppComponent, TodoComponent, TodoListComponent],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [DataService]
 })
 export class AppModule {}
--- src/app/store/todo.action.ts
+++ src/app/store/todo.action.ts
@@ -1,10 +1,28 @@
+import { HttpErrorResponse } from "@angular/common/http";
 import { Action } from "@ngrx/store";
+import { Todo } from "./models/todo";
 
+export enum TodoActionTypes {
+  ADD_TODO = "[TODO] add todo",
+  COMPLETE_TODO = "[TODO] completed todo",
+  DELETE_TODO = "[TODO] delete todo",
+  LOAD_TODO = "[TODO] load todos",
+  LOAD_TODO_SUCCESS = "[TODO] load todos success",
+  LOAD_TODO_FAILURE = "[TODO] load todos failure"
+}
 
-export enum TodoActionTypes  {
-  ADD_TODO = '[TODO] add todo',
-  COMPLETE_TODO = '[TODO] completed todo',
-  DELETE_TODO = '[TODO] delete todo' 
+export class LoadTodoAction implements Action {
+  readonly type = TodoActionTypes.LOAD_TODO;
+}
+
+export class LoadTodoActionSuccess implements Action {
+  readonly type = TodoActionTypes.LOAD_TODO_SUCCESS;
+  constructor(payload: Todo[]) {}
+}
+
+export class LoadTodoActionFailure implements Action {
+  readonly type = TodoActionTypes.LOAD_TODO_FAILURE;
+  constructor(public payload: HttpErrorResponse) {}
 }
 
 export class AddTodoAction implements Action {
@@ -13,14 +31,18 @@ export class AddTodoAction implements Action {
 }
 
 export class CompleteTodoAction implements Action {
-  readonly type = TodoActionTypes.COMPLETE_TODO
+  readonly type = TodoActionTypes.COMPLETE_TODO;
   constructor(public payload: string) {}
 }
 
 export class DeleteTodoAction implements Action {
-  readonly type = TodoActionTypes.DELETE_TODO
+  readonly type = TodoActionTypes.DELETE_TODO;
   constructor(public payload: string) {}
 }
 
-
-export type TodoAction = AddTodoAction | CompleteTodoAction | DeleteTodoAction
+export type TodoAction =
+  | AddTodoAction
+  | CompleteTodoAction
+  | DeleteTodoAction
+  | LoadTodoActionSuccess
+  | LoadTodoActionFailure;
